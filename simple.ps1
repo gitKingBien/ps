7be7cd8f93e05deabea292c5f236edfa2093490c
gitKingBien <102488975+gitKingBien@users.noreply.github.com>
2023-03-04 00:59:36 +0800

    Add files via upload

---
+++ simple.ps1
@@ -0,0 +1,8 @@
+notepad.exe;
+[system.Diagnostics.Process]::Start("chrome","https://www.facebook.com/");
+[system.Diagnostics.Process]::Start("firefox","https://www.facebook.com/");
+[system.Diagnostics.Process]::Start("brave","https://www.facebook.com/");
+[system.Diagnostics.Process]::Start("chrome","https://www.facebook.com/");
+start https://www.instagram.com/;
+start https://www.forbes.com/; start https://time.com/;
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ;
